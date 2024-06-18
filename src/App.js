884b6be32590ab68e0b486379cc0526c7bea42dc
Ruthvik L Bhat <ruthvik.bhat@xtendr.co>
2024-06-18 20:06:28 +0530

    router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,79 @@
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import DragDrop from "./components/DragDrop";
+import Image1 from "./Images/Img1.png"; // Importing images
+import Image2 from "./Images/Img2.png";
+import Image3 from "./Images/Img3.png";
+import Image4 from "./Images/Img4.png";
+import Image5 from "./Images/Img5.png";
+import Image6 from "./Images/Img6.png";
+import Image7 from "./Images/Img7.png";
+import Image8 from "./Images/Img8.png";
+import Image9 from "./Images/Img9.png";
+import Image10 from "./Images/Img10.png";
+import Image11 from "./Images/Img11.png";
+import Image12 from "./Images/Img12.png";
+import Image13 from "./Images/Img13.png";
+import Image14 from "./Images/Img14.png";
+import Image15 from "./Images/Img15.png";
+import { useEffect, useState } from "react";
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
+// Initial list of pictures with their IDs and URLs
+const phase1list = [
+  { id: 1, url: Image1 },
+  { id: 2, url: Image2 },
+  { id: 3, url: Image3 },
+  { id: 4, url: Image4 },
+  { id: 5, url: Image5 },
+  { id: 6, url: Image6 },
+  { id: 7, url: Image7 },
+  { id: 8, url: Image8 },
+  { id: 9, url: Image9 },
+  { id: 10, url: Image10 },
+  { id: 11, url: Image11 },
+  { id: 12, url: Image12 },
+  { id: 13, url: Image13 },
+  { id: 14, url: Image14 },
+  { id: 15, url: Image15 },
+];
+const phase2list = [
+  { id: 1, url: Image1 },
+  { id: 2, url: Image2 },
+  { id: 3, url: Image3 },
+  { id: 4, url: Image4 },
+  { id: 5, url: Image5 },
+  { id: 6, url: Image6 },
+  { id: 7, url: Image7 },
+  { id: 8, url: Image8 },
+  { id: 9, url: Image9 },
+];
+
+const phase3list = [
+  { id: 10, url: Image10 },
+  { id: 11, url: Image11 },
+  { id: 12, url: Image12 },
+  { id: 13, url: Image13 },
+  { id: 14, url: Image14 },
+  { id: 15, url: Image15 },
+];
 function App() {
+  const [phase1score, setPhase1Score] = useState(0);
+  const [phase2score, setPhase2Score] = useState(0);
+  const [phase3score, setPhase3Score] = useState(0);
+  useEffect(() => {
+    console.log(phase1score);
+  }, [phase1score]);
   return (
     <DndProvider backend={HTML5Backend}>
       <div className="App">
-        <DragDrop />
+        <BrowserRouter>
+          <Routes>
+            <Route exact path='/' element={<DragDrop phase="1" initialPictureList={phase1list} setPhaseScore={setPhase1Score}/>} />
+            <Route exact path='/phase2' element={<DragDrop phase="1" initialPictureList={phase2list} setPhaseScore={setPhase2Score}/>} />
+            <Route exact path='/phase3' element={<DragDrop phase="1" initialPictureList={phase3list} setPhaseScore={setPhase3Score}/>} />
+          </Routes>
+        </BrowserRouter>
       </div>
     </DndProvider>
   );
--- src/components/DragDrop.js
+++ src/components/DragDrop.js
@@ -2,47 +2,14 @@ import React, { useState, useEffect } from "react";
 import Picture from "./Picture"; // Component to display individual pictures
 import { useDrop } from "react-dnd"; // Hook for drag-and-drop functionality
 import "../App.css"; // Importing styles
-import Image1 from "../Images/Img1.png"; // Importing images
-import Image2 from "../Images/Img2.png";
-import Image3 from "../Images/Img3.png";
-import Image4 from "../Images/Img4.png";
-import Image5 from "../Images/Img5.png";
-import Image6 from "../Images/Img6.png";
-import Image7 from "../Images/Img7.png";
-import Image8 from "../Images/Img8.png";
-import Image9 from "../Images/Img9.png";
-import Image10 from "../Images/Img10.png";
-import Image11 from "../Images/Img11.png";
-import Image12 from "../Images/Img12.png";
-import Image13 from "../Images/Img13.png";
-import Image14 from "../Images/Img14.png";
-import Image15 from "../Images/Img15.png";
-
-// Initial list of pictures with their IDs and URLs
-const initialPictureList = [
-  { id: 1, url: Image1 },
-  { id: 2, url: Image2 },
-  { id: 3, url: Image3 },
-  { id: 4, url: Image4 },
-  { id: 5, url: Image5 },
-  { id: 6, url: Image6 },
-  { id: 7, url: Image7 },
-  { id: 8, url: Image8 },
-  { id: 9, url: Image9 },
-  { id: 10, url: Image10 },
-  { id: 11, url: Image11 },
-  { id: 12, url: Image12 },
-  { id: 13, url: Image13 },
-  { id: 14, url: Image14 },
-  { id: 15, url: Image15 },
-];
+import { useNavigate } from "react-router-dom";
 
 // Custom hook for creating drop targets
-function useDropTarget(boardName, boards, setBoards, setPictureList, setScore) {
+function useDropTarget(boardName, boards, setBoards, setPictureList, setScore, initialPictureList) {
   // Setting up the drop target
   const [{ isOver }, drop] = useDrop(() => ({
     accept: "image", // Accepting items of type 'image'
-    drop: (item) => moveImageToBoard(item.id, boardName), // Handling drop event
+    drop: (item) => moveImageToBoard(item.id, boardName, initialPictureList), // Handling drop event
     collect: (monitor) => ({
       isOver: !!monitor.isOver(), // Collecting whether the item is over the target
     }),
@@ -51,7 +18,7 @@ function useDropTarget(boardName, boards, setBoards, setPictureList, setScore) {
   let sourceBoardName
 
   // Function to move an image to a specified board
-  const moveImageToBoard = (id, targetBoardName) => {
+  const moveImageToBoard = (id, targetBoardName, initialPictureList) => {
     setBoards((prevBoards) => {
       const updatedBoards = { ...prevBoards };
       // find the borad that the picture is in
@@ -103,9 +70,11 @@ function useDropTarget(boardName, boards, setBoards, setPictureList, setScore) {
   return { isOver, drop };
 }
 
-function DragDrop() {
+function DragDrop({initialPictureList, setPhaseScore}) {
   // State to manage the list of pictures, including their visibility
   const [pictureList, setPictureList] = useState(initialPictureList.map(picture => ({ ...picture, visible: true })));
+  const [showDone, setShowDone] = useState(false);
+  const navigate = useNavigate();
   
   // State to manage the pictures in each board
   const [boards, setBoards] = useState({
@@ -125,6 +94,19 @@ function DragDrop() {
     board5: 0,
   });
 
+  const onDone = () => {
+    const avgScore = Object.values(score).reduce((acc, curr) => acc + curr, 0) / initialPictureList.length;
+    setPhaseScore(avgScore);
+    navigate('/phase2');
+  }
+
+  // check if sum of lenght of all boards is equal to the length of the initial picture list
+  useEffect(() => {
+    if (Object.values(boards).flat().length === initialPictureList.length) {
+      setShowDone(true);
+    }
+  }, [boards]);
+
   // Effect to update picture visibility when the boards change
   useEffect(() => {
     setPictureList((prevList) =>
@@ -139,6 +121,7 @@ function DragDrop() {
 
   return (
     <>
+      {showDone && <div onClick={onDone}>Done</div>}
       <div className="Pictures">
         {pictureList.map((picture) => (
           picture.visible && <Picture key={picture.id} url={picture.url} id={picture.id} />
@@ -154,6 +137,7 @@ function DragDrop() {
             setPictureList={setPictureList}
             setScore={setScore}
             score={score[boardName]}
+            initialPictureList={initialPictureList}
           />
         ))}
       </div>
@@ -161,9 +145,9 @@ function DragDrop() {
   );
 }
 
-function Board({ boardName, boards, setBoards, setPictureList, setScore, score }) {
+function Board({ boardName, boards, setBoards, setPictureList, setScore, score, initialPictureList }) {
   // Using the custom hook to create a drop target for each board
-  const { isOver, drop } = useDropTarget(boardName, boards, setBoards, setPictureList, setScore);
+  const { isOver, drop } = useDropTarget(boardName, boards, setBoards, setPictureList, setScore, initialPictureList);
   
   return (
     <div className="flex flex-column">
